a61eda48ce0247985ee99ac03d6e9ebb1d08189e
Toby Rumans <trumans@pivotal.io>
2018-07-19 16:31:10 -0700

    stupid directories
    Signed-off-by: Robert Neumann <rneumann@pivotal.io>

--- ci/tasks/test.sh
+++ ci/tasks/test.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
 cd cube-untap
     ./gradlew clean build
-    cp ./build/libs/cube-0.0.1-SNAPSHOT.jar compiledApp/cube.jar
+cd ../
+
+cp cube-untap/build/libs/cube-0.0.1-SNAPSHOT.jar compiledApp/cube.jar
